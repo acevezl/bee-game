fdee67d163fd9b33416e1aa27226db2cc71afcd3
Luis Arnaut <lou.arnaut@gmail.com>
2025-09-27 20:11:47 +0200

    Added tilesets for moth, hornet, dragonfly, and fly

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,7 +16,7 @@ const TILT_SMOOTH = 10
 var moving : bool = false;
 var idling : bool = true;
 
-# Bee direction: 1 - Forward, -1 Backwards
+# Bee direction: 1 - Forward, -1 Backwards, 0 Idle
 var bee_direction = 1; 
 
 func _ready():
@@ -24,42 +24,52 @@ func _ready():
 	
 func reset():
 	moving = false
-	idling = true
-
-# Method to ge the input played (left, right, top, down)
-# Pulled it out of the _physics_process, in case I need it in other places (tho unlikely ATM)
-func get_input():
-	# Returns an x,y direction
+	idling = true	
+	
+func _physics_process(delta):
+	
+	# Get Input Direction - Returns an x,y direction
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	# Normalize with speed constant
 	velocity = input_direction * SPEED
 	
-func _physics_process(delta):
-	get_input()
 	move_and_slide()
 	
-	# Calculate tilt based on vertical movement
-	# If bee flies up, then tilt up, if bee flies down, then tilt down
-	#var target_tilt_rad := 0.0
-	var v = clamp(velocity.y / SPEED, -1.0, 1.0)
-	# Flip horizontally when moving left/right (i.e., look forwards vs backwards)
-	if velocity.x == -SPEED:
-		bee_sprite.flip_h = true
-		bee_direction = -1
-	if velocity.x == SPEED:
-		bee_sprite.flip_h = false
-		bee_direction = 1
-	
-	var target_tilt_rad = deg_to_rad(MAX_TILT_DEGREES) * v * bee_direction
+	# Determine if the bee is moving forward (1) or backwards (-1), or idling (0)
+	bee_direction = Input.get_axis("ui_left", "ui_right")
 
+	# Flip horizontally when moving left/right (i.e., look forwards vs backwards)
+	if bee_direction == 1:
+		bee_sprite.flip_h = false
+	if bee_direction == -1:
+		bee_sprite.flip_h = true # flipped
+	
+	
+	# Animate Idle vs "Run" (i.e., Fly)
+	if abs(velocity.x) > 0:
+		# Bee is moving left or right, animate flight
+		bee_sprite.play("fly")
+	else:
+		# Bee is idle, do not animate
+		bee_sprite.play("idle")
+	
+	# Tilt the bee up and down
+	# If bee flies up, then tilt up, if bee flies down, then tilt down
+	var target_tilt_rad : float = 0.0
+	var v = clamp(velocity.y / SPEED, -1.0, 1.0)
+	target_tilt_rad = deg_to_rad(MAX_TILT_DEGREES) * v * bee_direction
 	bee_sprite.rotation = lerp_angle(
 		bee_sprite.rotation,
 		target_tilt_rad,
 		clamp(TILT_SMOOTH * delta, 0.0, 1.0)
 	)
-
 	# Snap to whole pixels for crisp pixel art
 	position = position.round()
+	
+	
+	
+	
 	
 	# Default Godot code below, to be deleted
 	# Add the gravity.
